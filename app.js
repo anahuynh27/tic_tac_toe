20aa698a58ef2e1b889fcfe5cf14aee81e537e09
Ana Tran <ana.tran323@gmail.com>
2022-11-03 21:21:13 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const gameState = {
+    players: ['x', 'o'],
+    board: [
+      [null, null, null],
+      [null, null, null],
+      [null, null, null]
+    ]
+  }
+
+let playerX = gameState.players[0];
+console.log(playerX)
+let playerO = gameState.players[1];
+console.log(playerO)
+
+
+function addXHere(event) {
+  
+}
