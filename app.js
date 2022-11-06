72b844dce1a2d265d2105d6f2e974dac20bc4a21
Ana Tran <ana.tran323@gmail.com>
2022-11-06 10:41:47 -0600

    render/listener function

--- app.js
+++ app.js
@@ -12,7 +12,34 @@ console.log(playerX)
 let playerO = gameState.players[1];
 console.log(playerO)
 
+const table = document.getElementById("table"); //same as board?
 
-function addXHere(event) {
+
+//state
+let state; 
+function buildInitialState() {
   
 }
+
+//render
+function renderState() {
+  
+}
+//listeners
+function onBoardClick() {
+  //update state, doze or so helper functions here
+
+  function addXHere(event) {
+    console.log("click");
+    let target = event.target; //either <td> or <table> depending where you click
+    if (event.target.tagName == "TABLE") {
+      target.className = null //Change .className when you figure out what you need to put in there
+      console.log("this is null")
+       } 
+  }
+  
+  renderState()//shouw the user the new state
+}
+const board = document.getElementById("board")
+board.addEventListener("click", onBoardClick)
+
