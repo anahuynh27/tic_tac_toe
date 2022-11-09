198a81ed71a85cdd91a493a4c07a5930071c0942
Ana Tran <ana.tran323@gmail.com>
2022-11-08 21:14:19 -0600

    prmpt for name

--- app.js
+++ app.js
@@ -11,50 +11,48 @@ let playerX = gameState.players[0];
     console.log(playerX)
 let playerO = gameState.players[1];
     console.log(playerO)
-    
+  
+let random = Math.floor(Math.random())
+console.log(random)
 const table = document.getElementById("table"); //same as board?
-    
-//RESET BUTTON
-let reset = document.getElementById("reset");
-function resetFn() {
-  location.reload()
-}
-reset.addEventListener("click", resetFn)
 
-//MORE GAMES BUTTON
-let moreGames = document.getElementById("moreGames")
-function moreGamesFn() {
-  window.open("https://playtictactoe.org/")
-}
-moreGames.addEventListener("click", moreGamesFn)
-
-//state
-let state; 
-function buildInitialState() {
-  
-}
-
-//render
-function renderState() {
-  
-}
-//listeners
-function onBoardClick() {
-  //update state, doze or so helper functions here
-
-  
-  renderState()//shouw the user the new state
-}
 // const board = document.getElementById("board")
 // board.addEventListener("click", onBoardClick)
 
-//WRITE YOUR CODE BELOW
+//******* FUNCTIONS BELOW WORKED  ************/
+let goButton = document.getElementById("go")
+let selectPlayerAmount = document.getElementById("playerSelect")
+let playerOne = document.getElementById("first-player")
+let playerTwo = document.getElementById("player-two")
+//we're going to have <p id="instruction"> change .innerText with player name
+function goButtonFn(e) {
+  let target = e.target;
+  let oneOrTwo = selectPlayerAmount.value
+  let nameDisplay = document.getElementById("pVp")
+  if (oneOrTwo == "One Player") {
+    console.log("one")
+    let pNameOne = prompt("Player One - Enter Name Below", "Player One")
+    nameDisplay.innerHTML(pNameOne)
+  } else if (oneOrTwo == "Two Players") {
+    console.log("two")
+    let ppNameOne = prompt("Player One - Enter Name Below", "Player One")
+    let ppNameTwo = prompt("Player Two - Enter Name Below", "Player Two")
+  } else {
+    console.log("didn't work")
+  }
+}
+goButton.addEventListener('click', goButtonFn)
+
+
+
+
+
 function addXHere(event) {
   let target = event.target; //either <td> or <table> depending where you click
   console.log(target)
   console.log("click");
   
-  if (event.target.tagName == "TABLE") {
+  if (target.tagName == "TABLE") {
     target.className = null //Change .className when you figure out what you need to put in there
     console.log("this is null")
   } else {
@@ -62,6 +60,33 @@ function addXHere(event) {
      }
 }
 table.addEventListener('click', addXHere)
+
+//*********MORE GAMES BUTTON**********
+let moreGames = document.getElementById("moreGames")
+function moreGamesFn() {
+  window.open("https://playtictactoe.org/")
+}
+moreGames.addEventListener("click", moreGamesFn)
+
+//*****************RESET BUTTON
+let reset = document.getElementById("reset");
+function resetFn() {
+  location.reload()
+}
+reset.addEventListener("click", resetFn)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //MOUSEOVER && i did this on css td:hover
 // let td = table.querySelectorAll("td");
@@ -77,7 +102,7 @@ table.addEventListener('click', addXHere)
 //   }
 // })
 
-let playerSelect = document.getElementById("playerSelect")
+
 
 
 
