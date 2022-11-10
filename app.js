04451bcc118324fe55cd07d8f924879d2608aa38
Ana Tran <ana.tran323@gmail.com>
2022-11-09 19:33:37 -0600

    prompt name, tbody, xo selection gone

--- app.js
+++ app.js
@@ -10,16 +10,20 @@ const gameState = {
 let playerX = gameState.players[0];
     console.log(playerX)
 let playerO = gameState.players[1];
-    console.log(playerO)
-  
-let random = Math.floor(Math.random())
-console.log(random)
-const table = document.getElementById("table"); //same as board?
+console.log(playerO)
+    
 
-// const board = document.getElementById("board")
-// board.addEventListener("click", onBoardClick)
+
+const table = document.getElementById("table"); //same as board?
+let td = table.querySelectorAll("td")
+
+
+// let random = Math.floor(Math.random() * td.length)
+// console.log(random)
 
 //******* FUNCTIONS BELOW WORKED  ************/
+
+//***********PROMPTS NAME********* */
 let goButton = document.getElementById("go")
 let selectPlayerAmount = document.getElementById("playerSelect")
 let playerOne = document.getElementById("first-player")
@@ -28,35 +32,61 @@ let nameDisplay = document.getElementById("pVp")
 //we're going to have <p id="instruction"> change .innerText with player name
 function goButtonFn() {
   let oneOrTwo = selectPlayerAmount.value
+
   if (oneOrTwo == "One Player") {
     console.log("one")
     let pNameOne = prompt("Player One - Enter Name Below", "Player One")
     console.log(pNameOne)
     nameDisplay.innerText = `${pNameOne} vs. Computer`
+    let randomStart = Math.floor(Math.random() * gameState.players.length)
+    if (randomStart == 0) {
+        alert(`${pNameOne} will go first`)
+    } else {
+        alert('Computer will go first')
+      }
   } else if (oneOrTwo == "Two Players") {
     console.log("two")
     let ppNameOne = prompt("Player One - Enter Name Below", "Player One")
     let ppNameTwo = prompt("Player Two - Enter Name Below", "Player Two")
     nameDisplay.innerText = `${ppNameOne} vs. ${ppNameTwo}`
+    let randomStart = Math.floor(Math.random() * gameState.players.length)
+    if (randomStart == 0) {
+        alert(`${ppNameOne} will go first`)
+    } else {
+        alert(`${ppNameTwo} will go first`)
+      }
   } else {
     console.log("didn't work")
+    alert("Please select one or two players. Then click 'go'")
   }
 }
 goButton.addEventListener('click', goButtonFn)
 
 
+//******randomly select order chosen for us by game*** */
 
 
 
+
+
+
+
+
+
+
+//*********PLACES X OR O IN TD******* */
 function addXHere(event) {
   let target = event.target; //either <td> or <table> depending where you click
   console.log(target)
   console.log("click");
   
   if (target.tagName == "TABLE") {
-    target.className = null //Change .className when you figure out what you need to put in there
+    target = null 
     console.log("this is null")
-  } else {
+  } else if (target.tagName == "TBODY") {
+    target = null
+    console.log("this is also null")
+  }else {
     target.innerHTML = playerX
      }
 }
